1db46562e0c83683a99c4ce35a7e0dd218d1e017
Adelemphii <demilitioowner@gmail.com>
2022-07-09 00:51:25 -0500

    add check to make sure its a mob

--- src/main/java/tech/adelemphii/randommobnames/events/EntitySpawnListener.java
+++ src/main/java/tech/adelemphii/randommobnames/events/EntitySpawnListener.java
@@ -1,5 +1,6 @@
 package tech.adelemphii.randommobnames.events;
 
+import org.bukkit.entity.Mob;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
@@ -9,6 +10,9 @@ public class EntitySpawnListener implements Listener {
 
     @EventHandler
     public void onEntitySpawn(EntitySpawnEvent event) {
+        if(!(event.getEntity() instanceof Mob)) {
+            return;
+        }
         GeneralUtils.generateEntityData(event.getEntity());
     }
 
